cd9d16af370ece97a03382e5305386fd6999490d
tylerchessa <tylerchessa@gmail.com>
2023-03-10 03:55:09 +0000

    create a listing

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -215,9 +215,26 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = []
+  for (let key in property) {
+    console.log(queryParams)
+    queryParams.push(property[key])
+  }
+ const queryString = `INSERT INTO properties (title, description, parking_spaces, number_of_bathrooms, number_of_bedrooms, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+`
+console.log(queryString, queryParams)
+  return pool
+  .query(queryString, queryParams)
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
